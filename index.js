1bd1231dffedd7654aa692f9157fe3b5794d3d96
Kush Agarwal <agrawalkush783@gmail.com>
2024-12-01 16:22:06 +0530

    changes

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const cartRoutes = require("./routes/cartRoutes");
-// const { authMiddleware } = require("./middleware/authMiddleware");
+const { authMiddleware } = require("./middleware/authMiddleware");
 const router = require("./routes/cartRoutes");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
 
-// app.use(authMiddleware);
+app.use(authMiddleware);
 
 app.use("/api/cart", cartRoutes);
 
